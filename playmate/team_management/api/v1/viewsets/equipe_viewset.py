12f3cf6e7a94d23ed16597df8eaf3092d965e4b3
lucas <lucas@singular.inf.br>
2023-06-10 15:00:23 -0300

    filtro equipe completa

--- playmate/team_management/api/v1/viewsets/equipe_viewset.py
+++ playmate/team_management/api/v1/viewsets/equipe_viewset.py
@@ -1,8 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
 from playmate.team_management.api.v1.serializers import EquipeSerializer
-from playmate.team_management.models import Equipe
-
+from playmate.team_management.models import Equipe, EquipeAtleta
+import json
 
 class EquipeViewset(ModelViewSet):
     queryset = Equipe.objects.all()
     serializer_class=EquipeSerializer
+
+
+
+    def filter_queryset(self, queryset):
+        qs = super(EquipeViewset, self).filter_queryset(queryset)
+        completed = json.loads(self.request.query_params.get('completed', False) )
+        print(completed)
+        equipes = []
+        if completed:
+            print(completed, completed, completed)
+            for equipe in Equipe.objects.all():
+                if EquipeAtleta.objects.filter(equipe=equipe).count() > 1:
+                    equipes.append(equipe.id)
+        else:
+            for equipe in Equipe.objects.all():
+                if EquipeAtleta.objects.filter(equipe=equipe).count() < 2:
+                    equipes.append(equipe.id)
+                    print(completed, completed)
+        return qs.filter(id__in=equipes)
