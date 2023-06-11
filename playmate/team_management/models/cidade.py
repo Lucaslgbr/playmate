f2b87996293753a691b026dd0d509e8ac31810fe
lucas <lucas@singular.inf.br>
2023-06-11 11:44:25 -0300

    str modelo de cidade e estado

--- playmate/team_management/models/cidade.py
+++ playmate/team_management/models/cidade.py
@@ -4,3 +4,7 @@ from django.db import models
 class Cidade(models.Model):
     nome = models.CharField(max_length=255)
     uf = models.ForeignKey('Estado', on_delete=models.DO_NOTHING)
+    
+
+    def __str__(self) -> str:
+        return self.nome
