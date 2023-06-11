98f1a8051c364a0866880ff58a654877251353d2
lucas <lucas@singular.inf.br>
2023-06-11 12:36:59 -0300

    sem login required

--- playmate/team_management/api/v1/viewsets/login_required_modelviewset.py
+++ playmate/team_management/api/v1/viewsets/login_required_modelviewset.py
@@ -3,8 +3,6 @@ from rest_framework import authentication, permissions
 from rest_framework.viewsets import ModelViewSet
 
 class LoginRequiredModelViewSet(ModelViewSet):
-    authentication_classes = (authentication.SessionAuthentication,authentication.TokenAuthentication)
-    permission_classes = (permissions.IsAuthenticated,)
 
     def filter_by_or_operator(self,queryset,query_params):
         _queryset=queryset.none()
